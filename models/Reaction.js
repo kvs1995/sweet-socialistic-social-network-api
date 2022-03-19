8b605dc515745872bcff3f7ce4085804a67714f5
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-03-19 18:17:26 -0500

    added in models/Reactions.js pseudocode

---
+++ models/Reaction.js
@@ -0,0 +1,19 @@
+//require schema and types from mongoose
+
+// reactionSchema define
+
+  //reactionId - type: Schema.Types.ObjectId, default: method to create new ObjectId... () => new Types.ObjectId()?
+
+  //reactionBody - type: string, required: true, max_length: 280
+
+  //username - type: string, required: true
+
+  //createdAt - type: Date, default: Date.now(), get mehod to reformat the timestamp
+
+  //toJSON
+
+  //id: false (sense this is not a model)
+
+//module.exports = schema
+
+
