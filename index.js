77d313f09f3cb69721cb3e1a27c4ee0b86108c98
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-03-19 18:37:08 -0500

    index.js code to start app, and .gitignore

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 //require express
-
+const express = require('express');
 //require files for connection and routes
-
+const db = require('./config/connection')
+const routes = require('./routes')
 //set variables for  PORT = 3001 , app = express()
-
+const PORT = process.env.PORT || 3001;
+const app = express();
 //app middleware
-
   // url encoded
-  
+  app.use(express.urlencoded({ extended: true }));
   // express.json()
-
+  app.use(express.json());
   //routes
+  app.use(routes);
 
 //listen to PORT once db is open
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server is running on port ${PORT}`)
+  })
+});
