50ac54c1a0598ae32d6ad245c60bc9e56bb5d5ce
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-03-19 18:20:55 -0500

    code for config/connection.js - database name: socialsDB.

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,15 @@
 //require connect and connection via mongoose
+const { connect, connection } = require('mongoose')
 
 //create the connectionString (socialsDB)
+const connectionString = 
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/socialsDB';
 
 //connect the string 
+connect(connectionString, {
+  useNewUrlParser:true,
+  useUnifiedTopology: true,
+});
 
 //export
+module.exports = connection;
