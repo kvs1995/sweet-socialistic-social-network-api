689f1497bf7d95a9bb4f4faece5621e92b718e8c
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-03-19 20:11:53 -0500

    adding in the model and schema code for User.js model

--- models/User.js
+++ models/User.js
@@ -1,26 +1,54 @@
 //require schema and model from mongoose
-
+const { Schema, model } = require('mongoose');
 //Schema to create User model
-
+const userSchema = new Schema(
+  {
   //username - type: string, required: true, unique: true, trim: true
-
+    username: {
+      type: String,
+      required:true,
+      unique: true,
+      trim: true,
+    },
  //email - type: string, required: true, unique: true, match: look up regex for emails from Reg Ex homework. 
-
+    email: {
+      type:  String,
+      required: true,
+      unique: true,
+      match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+    },
   //thoughts 
-  
-   //list of __id values referencing thought model 
+    thoughts: [
+    //list of __id values referencing thought model 
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
   
   //friends 
-
+    friends: [
     //list of _id values referencing the User model (self-reference)
-
-
-  //toJSON
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Friend',
+      },
+    ],
+  },
+  { 
+    //toJSON 
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
 
 //declare a virtual, friendCount, that retrieves length of the user's friends array field on query 
   //Need to make this a function method that returns the friends length within the user. 
 
 //declare User as a model that uses the userSchema defined above. 
-
+const User = model('user', userSchema)
 //export User;
+
+module.exports = User;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
 // require the User and Thoughts 
-
+const User = require('./User')
+const Thought = require('./Thought')
 //export
+moddule.exports = { Student, Course }
