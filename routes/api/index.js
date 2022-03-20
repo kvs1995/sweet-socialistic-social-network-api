7f82481cd4ef67a0c4b681075da1128d7ec84d5a
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-03-19 23:10:13 -0500

    api/index.js routes to the user and thought routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,12 @@
 //import router
+const router = require('express').Router();
 
 //grab the routes in folder
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes.js');
 
 //middleware for routes
-
+router.use('/users', userRoutes);
+router.use('/thoughts')
 module.exports = router;
 
