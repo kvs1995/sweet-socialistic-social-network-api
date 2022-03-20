4c06f61ea0b2c1a2b4046b5c48d8217419313067
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-03-19 20:50:10 -0500

    Reaction.js schema

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,19 +1,42 @@
 //require schema and types from mongoose
+const { Schema, model } = require('mongoose');
 
 // reactionSchema define
-
-  //reactionId - type: Schema.Types.ObjectId, default: method to create new ObjectId... () => new Types.ObjectId()?
-
+const reactionSchema = new Schema (
+  {
+    //reactionId - type: Schema.Types.ObjectId, default: method to create new ObjectId... () => new Types.ObjectId()?
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
   //reactionBody - type: string, required: true, max_length: 280
-
+    reactionBody: {
+      type: string,
+      required: true, 
+      max_length: 280,
+    },
   //username - type: string, required: true
-
-  //createdAt - type: Date, default: Date.now(), get mehod to reformat the timestamp
-
+    username: {
+      type: String, 
+      required: true,
+    },
+  //crea tedAt - type: Date, default: Date.now(), get mehod to reformat the timestamp
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => date.toLocaleString(),
+    }
+  },
+  {
   //toJSON
-
+    toJSON: {
+      getters:true,
+    },
   //id: false (sense this is not a model)
+    id: false
+  }
+);
 
 //module.exports = schema
-
+module.exports = reactionSchema;
 
