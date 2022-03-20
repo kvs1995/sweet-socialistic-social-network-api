e7e44202f972c195ead4be39dd7127145ad1b1d9
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-03-19 23:08:02 -0500

    user Controllers for GET all, GET by ID, post a new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,30 @@
 // import User and Thought models from the model fikles
-
+const { User, Thought } = require('../models');
 //module.export = 
+module.export = {
   // GET all users
-
+  getUsers(req,res) {
+    User.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(500).json(err));
+  },
   // GET a single user by its _id and populated thought and friend data
-
+  getSingleUser(req,res) {
+    User.findOne({ _id: req.params.userId})
+    .populate('thoughts')
+    .populate('friends')
+    .then((user) => 
+      !user 
+        ? res.status(404).jon({ message: 'No user with that Id!'})
+        : res.json(user))
+    .catch((err) => res.status(500).json(err));
+  },
   // POST a new user
-
+  createUser(req,res) {
+    User.create(req.body)
+    .then ((dbUserData) => res.json(dbUserData))
+    .catch((err) => res.status(500).json(err));
+  }
   // PUT to update a user by its _id
 
   // DELETE to remove user by its _id. 
@@ -18,3 +36,6 @@
 
   // DELETE to remove a friend from a user's friend list. 
 
+
+}
+  
---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+//import router
+
+//grab the routes in folder
+
+//middleware for routes
+
+module.exports = router;
+
